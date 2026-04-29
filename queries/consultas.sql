2880e484621969453e4ca3bcdd1e99ced38db80d
MayeLabs <mayeaguilera96@gmail.com>
2026-04-29 05:51:37 -0500

    Queries - Part II

--- queries/consultas.sql
+++ queries/consultas.sql
@@ -33,3 +33,28 @@ FROM productos p
 ORDER BY p.precio DESC 
 
 
+/* *** PARTE II *** */
+
+/* Ejercicio 1: 
+    ¿Cuántos clientes tenemos registrados en total? */
+SELECT COUNT(*) AS cliente_total
+FROM clientes c
+
+
+SELECT COUNT(c.cliente_id) 
+FROM clientes c
+
+/* Ejercicio 2: 
+    ¿Cuál es el precio del producto más caro que vendemos?*/
+SELECT MAX(p.price)
+FROM productos p 
+
+/* Ejercicio 3: 
+    ¿Cuál es el valor total promedio de los pedidos realizados?*/
+SELECT AVG(ped.total)
+FROM pedidos ped
+
+/* Ejercicio 4: 
+    Cuánto dinero ha ingresado la tienda en total (suma de la columna total en la tabla pedidos)? */
+SELECT SUM(ped.total)
+FROM pedidos ped
